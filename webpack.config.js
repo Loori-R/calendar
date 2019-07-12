7f697907c7921c9b8c6e03cb0fd9c2c02c0dc536
Loori-R <Loor@list.ru>
2019-07-13 02:12:21 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: __dirname + "/src/app/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: [/node_modules/]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/public/index.html",
+      inject: "body"
+    })
+  ],
+  devServer: {
+    contentBase: "./src/public",
+    port: 7700
+  }
+};
