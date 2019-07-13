f45b8bb9141a989cbe17d5484761e492e92bf233
Loori-R <Loor@list.ru>
2019-07-14 02:38:04 +0300

    feat: add weakdays and table size

---
+++ src/app/calendar/settings.js
@@ -0,0 +1,15 @@
+const weekdays = [
+  "воскресенье",
+  "понедельник",
+  "вторник",
+  "среда",
+  "четверг",
+  "пятница",
+  "суббота"
+];
+
+const rows = 5;
+const colums = 7;
+const square = rows * colums;
+
+export {weekdays,square}
