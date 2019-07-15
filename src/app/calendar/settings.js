ef5a353c69c2feb8e970b3a13442cc1903b87470
Loori-R <Loor@list.ru>
2019-07-16 00:53:49 +0300

    refact: move weekdays to class DayInfo

--- src/app/calendar/settings.js
+++ src/app/calendar/settings.js
@@ -1,15 +1,5 @@
-const weekdays = [
-  "воскресенье",
-  "понедельник",
-  "вторник",
-  "среда",
-  "четверг",
-  "пятница",
-  "суббота"
-];
-
 const rows = 5;
 const colums = 7;
 const square = rows * colums;
 
-export {weekdays,square}
+export {square}
