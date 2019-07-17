413290ab0c25c816066f448812033bbffa2e28e1
Loori-R <Loor@list.ru>
2019-07-18 00:37:35 +0300

    feat: add btns

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -1,9 +1,13 @@
 import "./header.css";
-import fastAddEvent from "./fastAddEvent";
+import btnFastAdd from "./fastAdd/btnFastAdd";
+import btnUpdate from "./update/btnUpdate";
+import inputSearch from "./seacrh/inputSearch";
 
 const createHeader = () => {
   const header = document.createElement("header");
-  header.insertAdjacentElement("afterbegin", fastAddEvent());
+  header.insertAdjacentElement("afterbegin", btnFastAdd);
+  header.insertAdjacentElement("beforeend", btnUpdate);
+  header.insertAdjacentElement("beforeend", inputSearch);
   return header;
 };
 export default createHeader;
